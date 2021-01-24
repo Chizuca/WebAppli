74a62ceb77ceebaffb5ea60cf88681f0f170f369
Chizuca <lucie.fribault@e-artsup.net>
2021-01-24 20:36:49 +0100

    drawn effect( touchmove)

--- JS/game.js
+++ JS/game.js
@@ -28,18 +28,32 @@ body.addEventListener('touchstart',(e) =>{
     nvCercle.style.borderRadius = '50%';
     nvCercle.style.top = '0px';
     nvCercle.style.left = '0px';
+    
     let posX = (e.touches[0].clientX - 25);
     let posY = (e.touches[0].clientY - 25);
-
     nvCercle.style.transform = `translate(${posX}px, ${posY}px)`;
     body.appendChild(nvCercle);
+})
+body.addEventListener('touchmove',(i) =>{
+    let nvCercle = document.createElement('div');
+    nvCercle.style.width = '20px';
+    nvCercle.style.height = '20px';
+    nvCercle.style.background = 'black';
+    nvCercle.style.position = 'absolute';
+    nvCercle.style.borderRadius = '50%';
+    nvCercle.style.top = '0px';
+    nvCercle.style.left = '0px';
     
+    let posX = (i.touches[0].clientX - 10);
+    let posY = (i.touches[0].clientY - 10);
+    nvCercle.style.transform = `translate(${posX}px, ${posY}px)`;
+    body.appendChild(nvCercle);
 })
 function gameLoop(){
     //fonction qui permet d'avoir un fond gris
     ctx.fillStyle = "grey";
     ctx.fillRect(0,0,canvas.width,canvas.height);
-
+//Fait apparaitre un rectangle violet 
     ctx.fillStyle = "purple";
     ctx.fillRect(x,y,50,50);
     if((x+50+movex >= canvas.width)||(x+movex <= 0))
@@ -54,20 +68,4 @@ function gameLoop(){
     y += movey;
 }
 setInterval(gameLoop,1000/60);
-function createCircle(){
 
-    let nvCercle = document.createElement('div');
-    nvCercle.style.width = '50px';
-    nvCercle.style.height = '50px';
-    nvCercle.style.background = 'red';
-    nvCercle.style.position = 'absolute';
-    nvCercle.style.borderRadius = '50%';
-    nvCercle.style.top = '0px';
-    nvCercle.style.left = '0px';
-    let posX = (e.touches[0].clientX - 25);
-    let posY = (e.touches[0].clientY - 25);
-
-    nvCercle.style.transform = `translate(${posX}px, ${posY}px)`;
-    body.appendChild(nvCercle);
-    
-}
